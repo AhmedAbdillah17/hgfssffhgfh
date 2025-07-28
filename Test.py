1c67c6097f7a6cbe0ced2e93f2caad9c0b8351c0
AhmedAbdillah17 <ahmednabdillah1998@gmail.com>
2025-07-28 12:28:41 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -19,7 +19,8 @@ USERS = ["MQ", "Samo", "Bashe"]
 # ---------------------
 # GOOGLE SHEETS AUTH (SECURE)
 # ---------------------
-credentials = st.secrets["gcp_service_account"]  # ✅ FIXED
+credentials = dict(st.secrets["gcp_service_account"])
+credentials["private_key"] = credentials["private_key"].replace("\\n", "\n")  # ✅ Fix for newline issue
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
 client = gspread.authorize(creds)
